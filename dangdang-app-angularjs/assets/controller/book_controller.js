ecbc46d3368b4090ad633c1af73e379c0a7ad001
ShenLiPeng <mac@Fangshuixin.local>
2016-10-19 21:52:32 +0800

    还

--- dangdang-app-angularjs/assets/controller/book_controller.js
+++ dangdang-app-angularjs/assets/controller/book_controller.js
@@ -1,9 +1,9 @@
 app.controller('bookController',['$scope',function($scope){
-    $scope.bookTypes= [
-        {id:'ertong',name:' 儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:''},
-        {id:'lishi',name:' 历史',img:'http://img3x5.ddimg.cn/24/35/24025245-1_l_4.jpg',description:''},
-        {id:'dongman',name:'动漫',img:'http://img3x1.ddimg.cn/7/20/24036811-1_l_5.jpg',description:''},
-        {id:'qingchunwenxue',name:'青春文学',img:'http://img3x5.ddimg.cn/35/6/24024365-1_l_12.jpg',description:''},
+    $scope.bookTypes = [
+        {id:'ertong',name:'儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:'当当儿童'},
+        {id:'lishi',name:' 历史',img:'http://img3x5.ddimg.cn/24/35/24025245-1_l_4.jpg',description:'当当网历史'},
+        {id:'dongman',name:'动漫',img:'http://img3x1.ddimg.cn/7/20/24036811-1_l_5.jpg',description:'当当网动漫'},
+        {id:'qingchunwenxue',name:'青春文学',img:'http://img3x5.ddimg.cn/35/6/24024365-1_l_12.jpg',description:'当当网青春文学'},
 
 
     ]
