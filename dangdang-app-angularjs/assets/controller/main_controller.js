bf0f5314c79c0e9b9b78db62292adbc4cc27d14c
ShenLiPeng <mac@Fangshuixin.local>
2016-10-19 15:37:57 +0800

    初始化

--- dangdang-app-angularjs/assets/controller/main_controller.js
+++ dangdang-app-angularjs/assets/controller/main_controller.js
@@ -1,5 +1,5 @@
-app.controller('mainController',['$scope','commonSerevice',function($scope,c_s){
-  c_s.getDta('ertong',function(res){
-      console.log()
+app.controller('mainController',['$scope','commonService',function($scope,c_s){
+  c_s.getData('ertong',function(res){
+      console.dir(res);
   })   
 }])
