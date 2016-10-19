dbb79237a68ca8c5d5a1e3d7191b9635da05b4cb
ShenLiPeng <mac@Fangshuixin.local>
2016-10-19 22:32:19 +0800

    0

--- dangdang-app-angularjs/assets/controller/main_controller.js
+++ dangdang-app-angularjs/assets/controller/main_controller.js
@@ -1,7 +1,7 @@
 app.controller('mainController',['$scope','commonService',function($scope,c_s){
-  c_s.getData('ertong',function(res){
-      console.dir(res);
-  });
+//   c_s.getData('ertong',function(res){
+//       console.dir(res);
+//   });
   $scope.selectedIndex = 0;
   $scope.linkTo = function(index){
       $scope.selectedIndex = index;
