e9f3ca8796c54fdf9b0e6d09a8cb9f76485a5eea
ShenLiPeng <mac@Fangshuixin.local>
2016-10-19 17:39:43 +0800

    tabbar

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -1,1 +1,29 @@
 var app = angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+    .when('/',{
+        template:'./tpl/index.html',
+        controller:''
+    })
+    .when('./blog',{  
+        template:'../tpl/blog.html',
+        controller:'blogController'
+    })
+    .when('./book',{
+        template:'./tpl/book.html',
+        controller:'bookController'
+    })
+    .when('./about_me',{
+        template:'./tpl/index.html',
+        controller:''
+    })
+    .when('/book_list',{
+        templateUrl:'./assets/tpl/book_list.html',
+        controller:'booklistController'
+    })
+    .otherwise({
+         redirectTo:'/'
+    })
+
+}])
---
+++ dangdang-app-angularjs/assets/controller/book_controller.js
@@ -0,0 +1,10 @@
+app.controller('bookController',['$scope',function($scope){
+    $scope.bookType = [
+        {id:'ertong',name:' 儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:''},
+        {id:'lishi',name:' 历史',img:'http://img3x5.ddimg.cn/24/35/24025245-1_l_4.jpg',description:''},
+        {id:'dongman',name:'动漫',img:'http://img3x1.ddimg.cn/7/20/24036811-1_l_5.jpg',description:''},
+        {id:'qingchunwenxue',name:'青春文学',img:'http://img3x5.ddimg.cn/35/6/24024365-1_l_12.jpg',description:''},
+
+
+    ]
+}])
