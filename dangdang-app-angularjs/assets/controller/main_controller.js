9892d5f365094d6f0faf541dfaa92238dcafc882
ShenLiPeng <mac@Fangshuixin.local>
2016-10-26 11:57:23 +0800

    about_me

--- dangdang-app-angularjs/assets/controller/main_controller.js
+++ dangdang-app-angularjs/assets/controller/main_controller.js
@@ -2,7 +2,7 @@ app.controller('mainController',['$scope','commonService',function($scope,c_s){
 //   c_s.getData('ertong',function(res){
 //       console.dir(res);
 //   });
-  $scope.selectedIndex = 1;
+  $scope.selectedIndex = 0;
   $scope.linkTo = function(index){
       $scope.selectedIndex = index;
   } 
