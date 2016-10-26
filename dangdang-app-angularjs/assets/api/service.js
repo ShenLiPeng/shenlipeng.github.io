9ebc79ed3937cea0bcf39585afa4b4bf8f892a26
ShenLiPeng <mac@Fangshuixin.local>
2016-10-26 11:03:03 +0800

    add

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -1,17 +1,39 @@
-app.factory('commonService',['$http',function($http){
-    var service = {};
-    service.getData = function(type,callback){
-        $http({
-            url:'/dangdang-app-angularjs/data/book_' + type + '.json',
-            method:'get'
-        })
-        .then(function(res){
-            console.log('获取数据成功');
-            callback(res);
+// app.factory('commonService',['$http',function($http){
+//     var service = {};
+//     service.getData = function(type,callback){
+//         $http({
+//             url:'/dangdang-app-angularjs/data/book_' + type + '.json',
+//             method:'get'
+//         })
+//         .then(function(res){
+//             console.log('获取数据成功');
+//             callback(res);
 
-        },function(err){
-            console.log(err);
-        })
+//         },function(err){
+//             console.log(err);
+//         })
+//     }
+//     return service;
+// }])
+app.factory('commonService', ['$http', function($http) {
+    var service = {};
+    /**
+     * 根据分类信息取数据
+     * @param  {[type]} type     [分类信息]
+     * @param  {[type]} callBack [回调函数]
+     * @return {[type]}          [description]
+     */
+    service.getData = function(type, callBack) {
+        $http({
+                url: '/dangdang-app-angularjs/data/book_' + type + '.json',
+                method: 'get'
+            })
+            .then(function(res) {
+                console.log('获取数据成功')
+                callBack(res);
+            }, function(err) {
+                console.dir(err);
+            })
     }
     return service;
 }])
