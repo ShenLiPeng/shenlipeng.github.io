8b6a5b01bd71f4971672022fda27bc6be4924a67
ShenLiPeng <mac@Fangshuixin.local>
2016-10-26 10:55:32 +0800

    add

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -2,7 +2,7 @@ app.factory('commonService',['$http',function($http){
     var service = {};
     service.getData = function(type,callback){
         $http({
-            url:'/dangdang-app-angularjs/data/book_'+type+'.json',
+            url:'/dangdang-app-angularjs/data/book_' + type + '.json',
             method:'get'
         })
         .then(function(res){
